dda9787e69ac2fe1bd754e6a7b7d6b3733b5c1b7
DanielRamosss <danielramos36@gmail.com>
2025-10-28 09:15:34 -0300

    fixed answers

--- learningtools/src/utils/questionsScenesDatabase.js
+++ learningtools/src/utils/questionsScenesDatabase.js
@@ -6,12 +6,12 @@ const questions = [
     audioText: "What is this? I think I got an idea",
     level: "beginner",
     options: [
-      "What is this? I think I got an idea",
       "What is that? I think I got an idea",
       "What is this? I think I have an idea",
+      "What is this? I think I got an idea",
       "What is this? I think I get an idea"
     ],
-    correctIndex: 0,
+    correctIndex: 2,
     category: "Movies"
   },
   {
@@ -21,12 +21,12 @@ const questions = [
     audioText: "Okay, so we messed up, but the world didn't end. We rebuilt it better for some",
     level: "intermediate",
     options: [
-      "Okay, so we messed up, but the world didn't end. We rebuilt it better for some",
       "Okay, so we missed up, but the world didn't end. We rebuild it better for some",
+      "Okay, so we messed up, but the world didn't end. We rebuilt it better for some",
       "Okay, so we messed up, but the world didn't end. We rebuild it better for sum",
       "Okay, so we messed up, but the world didn't end. We rebuilt it better for sum"
     ],
-    correctIndex: 0,
+    correctIndex: 1,
     category: "Movies"
   },
   {
@@ -36,12 +36,12 @@ const questions = [
     audioText: "The genetics. Genetically improved humans that live like kings",
     level: "intermediate",
     options: [
-      "The genetics. Genetically improved humans that live like kings",
       "The genetics. Genetically improved humans that lived like kings",
       "The genetic. Genetically improved humans that live like kings",
-      "The genetics. Genetically improve humans that live like kings"
+      "The genetics. Genetically improve humans that live like kings",
+      "The genetics. Genetically improved humans that live like kings"
     ],
-    correctIndex: 0,
+    correctIndex: 3,
     category: "Movies"
   },
   {
@@ -66,12 +66,12 @@ const questions = [
     audioText: "Proposition 42 enables genetic enhancement for everyone",
     level: "advanced",
     options: [
-      "Proposition 42 enables genetic enhancement for everyone",
       "Proposition 42 enable genetic enhancement for everyone",
       "Proposition 42 enables genetics enhancement for everyone",
+      "Proposition 42 enables genetic enhancement for everyone",
       "Proposition 42 enables genetic enhancements for everyone"
     ],
-    correctIndex: 0,
+    correctIndex: 2,
     category: "Movies"
   },
   {
@@ -81,12 +81,12 @@ const questions = [
     audioText: "Fix my suit. I'll pay you triple. Deal. Triple free. Well, don't go to the genetic district",
     level: "advanced",
     options: [
-      "Fix my suit. I'll pay you triple. Deal. Triple free. Well, don't go to the genetic district",
       "Fix my suit. I'll pay you triple. Deal. Triple fee. Well, don't go to the genetic district",
       "Fix my suit. I'll pay you triple. Deal. Triple free. Well, don't go to the genetics district",
-      "Fix my suit. I'll pick you triple. Deal. Triple free. Well, don't go to the genetic district"
+      "Fix my suit. I'll pick you triple. Deal. Triple free. Well, don't go to the genetic district",
+      "Fix my suit. I'll pay you triple. Deal. Triple free. Well, don't go to the genetic district"
     ],
-    correctIndex: 0,
+    correctIndex: 3,
     category: "Movies"
   },
   {
@@ -96,12 +96,12 @@ const questions = [
     audioText: "Today's attack by the extremist anti-genetic squad is the bloodiest so far",
     level: "advanced",
     options: [
-      "Today's attack by the extremist anti-genetic squad is the bloodiest so far",
       "Today's attack by the extremist anti-genetics squad is the bloodiest so far",
+      "Today's attack by the extremist anti-genetic squad is the bloodiest so far",
       "Today's attack by the extremist anti-genetic squad is the bloody so far",
       "Today attack by the extremist anti-genetic squad is the bloodiest so far"
     ],
-    correctIndex: 0,
+    correctIndex: 1,
     category: "Movies"
   },
   {
@@ -111,12 +111,12 @@ const questions = [
     audioText: "They're going to frame the specialists. Well, we got to do something then",
     level: "intermediate",
     options: [
-      "They're going to frame the specialists. Well, we got to do something then",
       "They're going to frame the specialists. Well, we've got to do something then",
       "They're gonna frame the specialists. Well, we got to do something then",
-      "They're going to frame the specialists. Well, we got to do something than"
+      "They're going to frame the specialists. Well, we got to do something than",
+      "They're going to frame the specialists. Well, we got to do something then"
     ],
-    correctIndex: 0,
+    correctIndex: 3,
     category: "Movies"
   },
   {
@@ -126,12 +126,12 @@ const questions = [
     audioText: "He's the one behind all of this. We've cracked the enigma of human evolution",
     level: "advanced",
     options: [
+      "He's the one behind all of these. We've cracked the enigma of human evolution",
       "He's the one behind all of this. We've cracked the enigma of human evolution",
       "He's the one behind all of this. We've crack the enigma of human evolution",
-      "He's the one behind all of these. We've cracked the enigma of human evolution",
       "He's the one behind all of this. We've cracked the enigma of human revolution"
     ],
-    correctIndex: 0,
+    correctIndex: 1,
     category: "Movies"
   },
   {
@@ -141,12 +141,12 @@ const questions = [
     audioText: "Question is, what are we going to do about it? We're going to fight",
     level: "intermediate",
     options: [
-      "Question is, what are we going to do about it? We're going to fight",
       "Question is, what are we gonna do about it? We're going to fight",
       "Question is, what are we going to do about it? We're gonna fight",
-      "Question is, what we going to do about it? We're going to fight"
+      "Question is, what we going to do about it? We're going to fight",
+      "Question is, what are we going to do about it? We're going to fight"
     ],
-    correctIndex: 0,
+    correctIndex: 3,
     category: "Movies"
   },
   {
@@ -156,12 +156,12 @@ const questions = [
     audioText: "They want you to think that you're useless, worthless. It's not true",
     level: "intermediate",
     options: [
-      "They want you to think that you're useless, worthless. It's not true",
       "They want you to think that you're useless, worthless. It's not truth",
       "They want you to think that your useless, worthless. It's not true",
+      "They want you to think that you're useless, worthless. It's not true",
       "They want you to think that you're useless, worth less. It's not true"
     ],
-    correctIndex: 0,
+    correctIndex: 2,
     category: "Movies"
   },
   {
@@ -171,12 +171,252 @@ const questions = [
     audioText: "It's evolution and you can't stop it",
     level: "beginner",
     options: [
-      "It's evolution and you can't stop it",
       "It's revolution and you can't stop it",
       "It's evolution and you can't stopped it",
-      "Its evolution and you can't stop it"
+      "Its evolution and you can't stop it",
+      "It's evolution and you can't stop it"
+    ],
+    correctIndex: 3,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 1,
+    endTime: 4,
+    audioText: "Cubs are wild in pursuit of suspect",
+    level: "intermediate",
+    options: [
+      "Cubs are wild in pursuit of suspects",
+      "Cubs are wild in pursuit of suspect",
+      "Cubs are wild in pursuit of the suspect",
+      "Cubs are wild in pursuits of suspect"
+    ],
+    correctIndex: 1,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 3,
+    endTime: 7,
+    audioText: "Back off the keys. We got him. Rolling spike",
+    level: "intermediate",
+    options: [
+      "Back off the case. We got him. Rolling spike",
+      "Back of the keys. We got him. Rolling spike",
+      "Back off the keys. We got them. Rolling spike",
+      "Back off the keys. We got him. Rolling spike"
+    ],
+    correctIndex: 3,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 10,
+    endTime: 14,
+    audioText: "Sorry. Could you show me that clip again? Wasn't wearing my glasses",
+    level: "advanced",
+    options: [
+      "Sorry. Could you show me that clip again? I wasn't wearing my glasses",
+      "Sorry. Could you show me that clip again? Wasn't wearing my glasses",
+      "Sorry. Could you show me this clip again? Wasn't wearing my glasses",
+      "Sorry. Could you show me that clip again? Wasn't wear my glasses"
+    ],
+    correctIndex: 1,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 18,
+    endTime: 25,
+    audioText: "Fail at this case and I will split you up",
+    level: "intermediate",
+    options: [
+      "Fail at this case and I will split you out",
+      "Fail in this case and I will split you up",
+      "Fail at this case and I will split you up",
+      "Fail at this case and I will separate you up"
+    ],
+    correctIndex: 2,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 24,
+    endTime: 30,
+    audioText: "No snake has set foot in Zootopia in forever",
+    level: "intermediate",
+    options: [
+      "No snake has set foot in Zootopia for forever",
+      "No snake has set feet in Zootopia in forever",
+      "No snake has sat foot in Zootopia in forever",
+      "No snake has set foot in Zootopia in forever"
+    ],
+    correctIndex: 3,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 36,
+    endTime: 40,
+    audioText: "We're going to crack this case and prove we're the greatest partners of all time",
+    level: "advanced",
+    options: [
+      "We're going to crack this case and prove we're the greatest partner of all time",
+      "We're going to crack this case and prove we're the greatest partners of all time",
+      "We're going to crack this case and prove we are the greatest partners of all time",
+      "We're gonna crack this case and prove we're the greatest partners of all time"
+    ],
+    correctIndex: 1,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 39,
+    endTime: 46,
+    audioText: "Yeah. If you want to talk to a reptile, I am your gal",
+    level: "intermediate",
+    options: [
+      "Yeah. If you want to talk to a reptile, I am your girl",
+      "Yeah. If you want to talk to a reptile, I'm your gal",
+      "Yeah. If you want to talk with a reptile, I am your gal",
+      "Yeah. If you want to talk to a reptile, I am your gal"
+    ],
+    correctIndex: 3,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 45,
+    endTime: 52,
+    audioText: "When Zootopia was founded, this whole neighborhood got cut off from the rest of the town",
+    level: "advanced",
+    options: [
+      "When Zootopia was founded, this whole neighborhood got cut of from the rest of the town",
+      "When Zootopia was founded, this whole neighborhood got cut off from the rest of town",
+      "When Zootopia was founded, this whole neighborhood got cutted off from the rest of the town",
+      "When Zootopia was founded, this whole neighborhood got cut off from the rest of the town"
+    ],
+    correctIndex: 3,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 50,
+    endTime: 54,
+    audioText: "It's the only place you can get away with wearing a shirt and no pants",
+    level: "advanced",
+    options: [
+      "It's the only place you can get away with wearing a shirt and no pant",
+      "It's the only place you can get away wearing a shirt and no pants",
+      "It's the only place you can get away with wearing a shirt and no pants",
+      "It's the only place you can get away with wear a shirt and no pants"
+    ],
+    correctIndex: 2,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 55,
+    endTime: 60,
+    audioText: "Zootopia has a secret reptile population",
+    level: "intermediate",
+    options: [
+      "Zootopia has a secret reptile population",
+      "Zootopia has a secret reptile populations",
+      "Zootopia has the secret reptile population",
+      "Zootopia has a secret reptiles population"
     ],
     correctIndex: 0,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 68,
+    endTime: 73,
+    audioText: "Whatever you're tangled up in has to do with snakes. It's dangerous",
+    level: "advanced",
+    options: [
+      "Whatever you're tangled up in has to do with snakes. Its dangerous",
+      "Whatever you're tangled up in has to do with snake. It's dangerous",
+      "Whatever your tangled up in has to do with snakes. It's dangerous",
+      "Whatever you're tangled up in has to do with snakes. It's dangerous"
+    ],
+    correctIndex: 3,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 73,
+    endTime: 77,
+    audioText: "I want the fox and rabbit gone",
+    level: "beginner",
+    options: [
+      "I want the fox and rabbit go",
+      "I want the fox and rabbit gone",
+      "I want the fox and rabbits gone",
+      "I want a fox and rabbit gone"
+    ],
+    correctIndex: 1,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 77,
+    endTime: 79,
+    audioText: "Stop pulling my ears. Stop pulling my ears",
+    level: "beginner",
+    options: [
+      "Stop pulling my ear. Stop pulling my ear",
+      "Stop pull my ears. Stop pull my ears",
+      "Stop pulling my ears. Stop pulling my ear",
+      "Stop pulling my ears. Stop pulling my ears"
+    ],
+    correctIndex: 3,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 91,
+    endTime: 96,
+    audioText: "Snakes aren't the bad guys. This is our chance to set things right",
+    level: "advanced",
+    options: [
+      "Snakes aren't the bad guys. This is our chance to set thing right",
+      "Snakes aren't the bad guys. This is our chance to set things right",
+      "Snakes aren't the bad guys. This is our chance to sit things right",
+      "Snakes aren't the bad guy. This is our chance to set things right"
+    ],
+    correctIndex: 1,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 98,
+    endTime: 105,
+    audioText: "We took an oath to make the world a better place. And an innocent snake needs our help",
+    level: "advanced",
+    options: [
+      "We took an oath to make the world a better place. And an innocent snake need our help",
+      "We took an oath to make the world the better place. And an innocent snake needs our help",
+      "We took an oath to make the world a better place. And a innocent snake needs our help",
+      "We took an oath to make the world a better place. And an innocent snake needs our help"
+    ],
+    correctIndex: 3,
+    category: "Movies"
+  },
+  {
+    videoId: "sEgPQ7HKoBA",
+    startTime: 117,
+    endTime: 122,
+    audioText: "Hey, Flash. Flash 100yard dash. We need to get across town",
+    level: "advanced",
+    options: [
+      "Hey, Flash. Flash 100 yard dash. We need to get across town",
+      "Hey, Flash. Flash 100yard dash. We need to get across the town",
+      "Hey, Flash. Flash 100yard dash. We need to get across town",
+      "Hey, Flash. Flash hundred yard dash. We need to get across town"
+    ],
+    correctIndex: 2,
     category: "Movies"
   }
 ];
